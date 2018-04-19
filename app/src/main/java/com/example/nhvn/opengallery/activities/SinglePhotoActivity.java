119759e1757638356a7bef1ad70c1edbb040f6c6
nguyenhoangvannha <1512363@student.hcmus.edu.vn>
2018-04-19 17:40:29 +0700

    Replace PhotosActivity by PhotosFragment

--- app/src/main/java/com/example/nhvn/opengallery/activities/SinglePhotoActivity.java
+++ app/src/main/java/com/example/nhvn/opengallery/activities/SinglePhotoActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.widget.Toast;
-
 import com.example.nhvn.opengallery.R;
 import com.example.nhvn.opengallery.adapters.PhotoPagerAdapter;
 import com.example.nhvn.opengallery.data.Album;
