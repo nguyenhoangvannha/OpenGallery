9640eccf8d8c7a52dedf43b376e8a8ddc71fad30
nguyenhoangvannha <1512363@student.hcmus.edu.vn>
2018-04-20 18:01:59 +0700

    TouchImageView zoom image

--- app/src/main/java/com/example/nhvn/opengallery/adapters/PhotosPagerAdapter.java
+++ app/src/main/java/com/example/nhvn/opengallery/adapters/PhotosPagerAdapter.java
@@ -1,6 +1,7 @@
 package com.example.nhvn.opengallery.adapters;
 
 import android.content.Context;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
@@ -13,6 +14,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.nhvn.opengallery.activities.PhotosPagerActivity;
 import com.example.nhvn.opengallery.data.Album;
+import com.example.nhvn.opengallery.view.TouchImageView;
 
 import java.io.File;
 
@@ -32,34 +34,52 @@ public class PhotosPagerAdapter extends PagerAdapter {
 
     @Override
     public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
-        return view == ((ImageView) object);
+        return view == ((TouchImageView) object);
     }
 
     @NonNull
     @Override
     public Object instantiateItem(@NonNull final ViewGroup container, int position) {
-        final ImageView imageView = new ImageView(context);
-        imageView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+
+        ImageView imageView = new ImageView(context);
         Glide.with(context).load(new File(album.getPhotos().get(position))).into(imageView);
-        ((ViewPager) container).addView(imageView, 0);
-        imageView.setOnClickListener(new View.OnClickListener() {
+
+        TouchImageView touchImageView = new TouchImageView(context);
+        //touchImageView.setImageDrawable(imageView.getDrawable());
+        touchImageView.setScaleType(TouchImageView.ScaleType.CENTER_INSIDE);
+
+        //Glide.with(context).load(new File(album.getPhotos().get(position))).into(touchImageView);
+        touchImageView.setImageURI(Uri.fromFile(new File(album.getPhotos().get(position))));
+        ((ViewPager) container).addView(touchImageView, 0);
+        touchImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 ((PhotosPagerActivity) context).toggle();
             }
         });
-        imageView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                Toast.makeText(context, "x: " + event.getX() + " y: " + event.getY(), Toast.LENGTH_SHORT).show();
-                return true;
-            }
-        });
-        return imageView;
+
+//        final ImageView imageView = new ImageView(context);
+//        imageView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+//        Glide.with(context).load(new File(album.getPhotos().get(position))).into(imageView);
+//        ((ViewPager) container).addView(imageView, 0);
+//        imageView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                ((PhotosPagerActivity) context).toggle();
+//            }
+//        });
+//        imageView.setOnTouchListener(new View.OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View v, MotionEvent event) {
+//                Toast.makeText(context, "x: " + event.getX() + " y: " + event.getY(), Toast.LENGTH_SHORT).show();
+//                return true;
+//            }
+//        });
+        return touchImageView;
     }
 
     @Override
     public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
-        ((ViewPager) container).removeView((ImageView) object);
+        ((ViewPager) container).removeView((TouchImageView) object);
     }
 }
